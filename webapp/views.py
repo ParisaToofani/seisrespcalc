311fb7851057a132c10e569513daf41f8f950988
DESKTOP-JMMITFI\Parisa <toofaniparisa@gmail.com>
2021-08-23 22:26:43 +0430

    1- Added template

--- webapp/views.py
+++ webapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def main_project(request):
     return render(request, 'main.html')
+
+def response_calc(request):
+    return
